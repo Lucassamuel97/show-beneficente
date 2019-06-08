26c0d55d9fbf011bcd607ef119e9737f4a104274
Lucas Samuel <lukassamuka88@gmail.com>
2019-06-08 15:28:43 -0300

    First commit

---
+++ src/main/java/br/edu/utfpr/alunos/ShowBeneficenteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowBeneficenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowBeneficenteApplication.class, args);
+	}
+
+}
